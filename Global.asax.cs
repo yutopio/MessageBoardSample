ec889c1617ab8d4ef1e39dea3b9c8b1d90ffd308
竹井 悠人 <yuto.takei@basset.ai>
2020-05-31 22:58:57 +0900

    最初のコミット

---
+++ Global.asax.cs
@@ -0,0 +1,15 @@
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Sample
+{
+    public class MvcApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
