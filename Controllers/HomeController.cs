8e05f2aab1a23a1a1770975247e7e9bfb75531d6
竹井 悠人 <yuto.takei@basset.ai>
2020-06-02 16:56:51 +0900

    UIを設計した

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,8 +4,21 @@ namespace Sample.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(IndexModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ModelState.Clear();
+                ViewBag.Posted = true;
+            }
+
             return View();
         }
     }
---
+++ Models/IndexModel.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sample
+{
+    public class IndexModel
+    {
+        public MessageModel Input { get; set; }
+        public IEnumerable<MessageModel> Posted { get; set; }
+    }
+
+    public class MessageModel
+    {
+        [DisplayName("ハンドル名")]
+        [Required(ErrorMessage = "ハンドル名を入力してください。")]
+        [MaxLength(20, ErrorMessage = "ハンドル名が長すぎます。")]
+        public string Username { get; set; }
+
+        [DisplayName("メッセージ")]
+        [Required(ErrorMessage = "メッセージを入力してください。")]
+        [MaxLength(140, ErrorMessage = "メッセージが長すぎます。")]
+        public string Message { get; set; }
+
+        public DateTime Posted { get; set; }
+    }
+}
